3662eb94d0f18edb5e91b0dba8a313d4b16b0034
warrenchulls <warren.as123@gmail.com>
2017-01-13 03:41:03 -0500

    m1

---
+++ src/p2/P2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2;
+
+/**
+ *
+ * @author WARREN
+ */
+public class P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int a1(){return 0;}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
