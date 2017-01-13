c6b61acdac4e62651d89a8a8dcdbe46815f4c998
warrenchulls <warren.as123@gmail.com>
2017-01-13 03:41:53 -0500

    m2

--- src/p2/P2.java
+++ src/p2/P2.java
@@ -15,6 +15,7 @@ public class P2 {
      * @param args the command line arguments
      */
     public static int a1(){return 0;}
+    public static int a2(){return 0;}
     public static void main(String[] args) {
         // TODO code application logic here
         
